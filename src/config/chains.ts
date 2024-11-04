e7531f8909b4cae084247e3c8af8ac12d992c27c
darshitbhalodi5 <darshitbhalodi@gmail.com>
2024-11-04 17:40:50 +0530

    Added chain config for remaining chains

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -18,7 +18,7 @@ const chainConfig: ChainConfig = {
     name: "Ethereum Sepolia",
     rpcUrl: `https://eth-sepolia.g.alchemy.com/v2/${alchemyKey}`,
     nativeCurrency: {
-      name: "Sepolia Ether",
+      name: "Ethereum",
       symbol: "ETH",
       decimals: 18,
     },
@@ -38,8 +38,8 @@ const chainConfig: ChainConfig = {
     name: "Optimism Sepolia",
     rpcUrl: `https://opt-sepolia.g.alchemy.com/v2/${alchemyKey}`,
     nativeCurrency: {
-      name: "Optimism Sepolia Ether",
-      symbol: "ETH",
+      name: "Optimism",
+      symbol: "OP",
       decimals: 18,
     },
     blockexplorer: 'https://sepolia-optimism.etherscan.io/tx',
@@ -48,7 +48,7 @@ const chainConfig: ChainConfig = {
     name: "Base Sepolia",
     rpcUrl: `https://base-sepolia.g.alchemy.com/v2/${alchemyKey}`,
     nativeCurrency: {
-      name: "Base Sepolia Ether",
+      name: "Ethereum",
       symbol: "ETH",
       decimals: 18,
     },
@@ -68,7 +68,7 @@ const chainConfig: ChainConfig = {
     name: "Mode Sepolia",
     rpcUrl: `https://sepolia.mode.network`,
     nativeCurrency: {
-      name: "Mode Sepolia Ether",
+      name: "Ethereum",
       symbol: "ETH",
       decimals: 18,
     },
@@ -78,7 +78,7 @@ const chainConfig: ChainConfig = {
     name: "Zora",
     rpcUrl: `https://zora-mainnet.g.alchemy.com/v2/${alchemyKey}`,
     nativeCurrency: {
-      name: "Zora Ether",
+      name: "Ethereum",
       symbol: "ETH",
       decimals: 18,
     },
@@ -90,7 +90,7 @@ const chainConfig: ChainConfig = {
     nativeCurrency: {
       name: "Lisk",
       symbol: "LSK",
-      decimals: 8,
+      decimals: 18,
     },
     blockexplorer: 'https://blockscout.lisk.com/tx',
   },    
@@ -124,6 +124,116 @@ const chainConfig: ChainConfig = {
     },
     blockexplorer: 'https://blockscout.kroma.network/tx',
   },  
+  480: {
+    name: "World Chain",
+    rpcUrl: `https://worldchain-mainnet.g.alchemy.com/v2/${alchemyKey}`,
+    nativeCurrency: {
+      name: "World Coin",
+      symbol: "WLD",
+      decimals: 18,
+    },
+    blockexplorer: "https://worldscan.org/tx",
+  },
+  288: {
+    name: "Boba Network",
+    rpcUrl: "https://mainnet.boba.network",
+    nativeCurrency: {
+      name: "Boba",
+      symbol: "BOBA",
+      decimals: 18,
+    },
+    blockexplorer: "https://bobascan.com/tx",
+  },
+  185: {
+    name: "Mint Blockchain",
+    rpcUrl: "https://asia.rpc.mintchain.io",
+    nativeCurrency: {
+      name: "Mint",
+      symbol: "MINT",
+      decimals: 18,
+    },
+    blockexplorer: "https://explorer.mintchain.io/tx",
+  },
+  690: {
+    name: "Redstone",
+    rpcUrl: "https://rpc.redstonechain.com",
+    nativeCurrency: {
+      name: "Redstone",
+      symbol: "RED",
+      decimals: 18,
+    },
+    blockexplorer: "https://explorer.redstone.xyz/tx",
+  },
+  360: {
+    name: "Shape",
+    rpcUrl: "https://mainnet.shape.network",
+    nativeCurrency: {
+      name: "Shape",
+      symbol: "SHAPE",
+      decimals: 18,
+    },
+    blockexplorer: "https://shapescan.xyz/tx",
+  },
+  254: {
+    name: "Swan Chain",
+    rpcUrl: "https://mainnet-rpc.swanchain.org",
+    nativeCurrency: {
+      name: "Swan",
+      symbol: "SWAN",
+      decimals: 18,
+    },
+    blockexplorer: "https://swanscan.io/tx",
+  },
+  8866: {
+    name: "Superlumio",
+    rpcUrl: "https://mainnet.lumio.io",
+    nativeCurrency: {
+      name: "Superlumio",
+      symbol: "SUPER",
+      decimals: 18,
+    },
+    blockexplorer: "https://explorer.lumio.io/tx",
+  },
+  1750: {
+    name: "MetalL2",
+    rpcUrl: "https://rpc.metall2.com",
+    nativeCurrency: {
+      name: "Metal",
+      symbol: "METAL",
+      decimals: 18,
+    },
+    blockexplorer: "https://explorer.metall2.com/tx",
+  },
+  5112: {
+    name: "Ham Chain",
+    rpcUrl: "https://rpc.ham.fun",
+    nativeCurrency: {
+      name: "Ham",
+      symbol: "HAM",
+      decimals: 18,
+    },
+    blockexplorer: "https://explorer.ham.fun/tx",
+  },
+  2192: {
+    name: "SNAX Chain",
+    rpcUrl: "https://mainnet.snaxchain.io",
+    nativeCurrency: {
+      name: "SNAX",
+      symbol: "SNAX",
+      decimals: 18,
+    },
+    blockexplorer: "https://explorer.snaxchain.io/tx",
+  },
+  888888888: {
+    name: "Ancient 8",
+    rpcUrl: "https://rpc.ancient8.gg",
+    nativeCurrency: {
+      name: "Ancient",
+      symbol: "ANC8",
+      decimals: 18,
+    },
+    blockexplorer: "https://scan.ancient8.gg/tx",
+  },
 };
 
 export default chainConfig;
