f76d240adf575401e95796951c2abb3fb4987fde
Lajja123 <lajja.lamprostech@gmail.com>
2024-11-14 14:32:35 +0530

    Added metedata in send token page

---
+++ src/app/send-token/page.tsx
@@ -0,0 +1,38 @@
+import SentToken from "../../components/sendToken/page";
+import Image from "next/image";
+import img from "../assets/darkbg.png";
+
+export default function sendTo() {
+  return (
+    <div>
+      <div className="">
+        <SentToken />
+      </div>
+    </div>
+  );
+}
+export const metadata = {
+  title: "Send-Token",
+  description: "Gift token as easy as sending email",
+  openGraph: {
+    title: "Send-Token",
+    description: "Gift token as easy as sending email",
+    url: "https://hello-crypto.vercel.app/",
+    siteName: "CryptoCourier",
+    images: [
+      {
+        url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
