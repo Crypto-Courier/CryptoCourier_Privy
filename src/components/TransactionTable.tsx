3da18d2aeb1462d9cc28e38af3087f985cc1d46a
darshitbhalodi5 <darshitbhalodi@gmail.com>
2024-12-02 15:03:09 +0530

    New endpoint and try to fetch data using one api

--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -48,9 +48,11 @@ const TransactionTable: React.FC<TransactionTableProps> = ({
         
         const chainIdQuery = chainsToQuery.map(id => `chainId=${id}`).join("&");
         
-        const endpoint = viewMode === "dashboard"
-          ? `/api/get-dashboard-transaction?walletAddress=${activeAddress}`
-          : `/api/get-transactions?walletAddress=${activeAddress}&${chainIdQuery}`;
+        // const endpoint = viewMode === "dashboard"
+        //   ? `/api/get-dashboard-transaction?walletAddress=${activeAddress}`
+        //   : `/api/get-transactions?walletAddress=${activeAddress}&${chainIdQuery}`;
+
+        const endpoint = `/api/get-transactions?walletAddress=${activeAddress}&${chainIdQuery}`;
 
         const response = await fetch(endpoint);
         if (!response.ok) {
